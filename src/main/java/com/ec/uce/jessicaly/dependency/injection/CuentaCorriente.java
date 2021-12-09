571a7d1a95ad0d5867257a2132dbe372d8b7e307
jessicalyruiz <jessicalyr@gmail.com>
2021-12-09 18:53:22 -0500

    sistema bancario first

---
+++ src/main/java/com/ec/uce/jessicaly/dependency/injection/CuentaCorriente.java
@@ -0,0 +1,34 @@
+package com.ec.uce.jessicaly.dependency.injection;
+
+import java.math.BigDecimal;
+
+public class CuentaCorriente extends Cuenta{
+
+	private float montoMinimo;
+
+	@Override
+	public void aperturaCuenta(Cliente cliente, float saldo) {
+		super.aperturaCuenta(cliente,saldo);
+		super.interes=8/100.0f;
+		System.out.println("Cuenta de Corriente creada con exito");
+	}
+	
+	
+	
+	@Override
+	public String toString() {
+		return "CuentaCorriente [montoMinimo=" + montoMinimo + ", numeroCuenta=" + numeroCuenta + ", saldo=" + saldo
+				+ ", interes=" + interes + ", cuentaHabiente=" + cuentaHabiente + "]";
+	}
+
+	public float getMontoMinimo() {
+		return montoMinimo;
+	}
+
+	public void setMontoMinimo(float montoMinimo) {
+		this.montoMinimo = montoMinimo;
+	}
+	
+	
+	
+}
